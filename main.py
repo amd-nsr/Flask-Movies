2c0bc40577f38ea1c8b869ae059240933543c980
b-e-d-o <36853269+b-e-d-o@users.noreply.github.com>
2021-08-25 04:42:42 +0200

    for production

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def create_app():
     return app
 
 
-if __name__ == "__main__":
-    app = create_app()
-    port = app.config.get("PORT", 5000)
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    app = create_app()
+#    port = app.config.get("PORT", 5000)
+#    app.run(host="0.0.0.0", port=port)
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from main import app, create_app
+
+if __name__ == "__main__":
+    app = create_app()
+    port = app.config.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
